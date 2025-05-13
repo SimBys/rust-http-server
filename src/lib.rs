e09b721f0aaacfbed2952f8b0686e76f9b3b8113
SimBys <simon.bystricky@gmail.com>
2025-05-13 20:24:49 +0200

    Add other HTTP methods

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,16 @@
 pub mod handler;
+pub mod http_method;
 pub mod logger;
+pub mod middleware;
 pub mod request;
 pub mod response;
 pub mod router;
 pub mod server;
 
 pub use handler::Handler;
+pub use http_method::HttpMethod;
 pub use logger::Logger;
+pub use middleware::{Middleware, NextFn};
 pub use request::Request;
 pub use response::Response;
 pub use router::Router;
